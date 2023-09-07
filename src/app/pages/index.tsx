6ade61f7d7171e5fc3ea9ccdd9500ef6aea7b355
Dizzion <gofrodude_197@hotmail.com>
2023-09-06 22:17:32 -0700

    file structure created for nextjs

---
+++ src/app/pages/index.tsx
@@ -0,0 +1,10 @@
+import styles from './page.module.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      
+    </main>
+  )
+}
