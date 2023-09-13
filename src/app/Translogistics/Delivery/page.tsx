00c9b1690b33a74c284a418bafe4ad62cb5527ec
Dizzion <gofrodude_197@hotmail.com>
2023-09-13 05:45:29 -0700

    delivery started

--- src/app/Translogistics/Delivery/page.tsx
+++ src/app/Translogistics/Delivery/page.tsx
@@ -1,6 +1,31 @@
+import pb from '@/utils/pocketbase'
 import React from 'react'
 
-function Delivery() {
+export const dynamic = 'auto',
+  dynamicParams = true,
+  revalidate = 0,
+  fetchCache = 'auto',
+  runtime = 'nodejs',
+  preferredRegion = 'auto'
+
+async function getTrackingNumbers() {
+
+  const data = await pb.collection('TrackingNumbers').getFullList();
+  return data;
+}
+
+async function Delivery() {
+  const trackingNumbers = await getTrackingNumbers();
+
+  const updateAsDelivered = async (enteredTrackingNumber: string) => {
+    try {
+      await pb.collection('TrackingNumbers').update()
+    } catch (error) {
+      console.log(error);
+    }
+    
+  }
+
   return (
     <div>Delivery</div>
   )
