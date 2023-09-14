74ad1bf59b2b18d22fbb392b8e4c51934f6ba450
Dizzion <dizzander43@gmail.com>
2023-09-14 15:02:09 -0700

    changed to don't scan empty totes

--- src/_components/OutboundForm.tsx
+++ src/_components/OutboundForm.tsx
@@ -20,7 +20,6 @@ const OutboundForm: React.FC<OutboundFormProps> = ({
   const [tnIds, setTnIds] = useState<string[]>([]);
   const [enteredSapTotes, setEnteredSapTotes] = useState<RecordModel[]>([]);
   const [emptyTotes, setEmptyTotes] = useState<string[]>([]);
-  const [from99To133, setFrom99To133] = useState(false);
   const [enteredTrackingNumbers, setEnteredTrackingNumbers] = useState<
     RecordModel[]
   >([]);
@@ -38,8 +37,8 @@ const OutboundForm: React.FC<OutboundFormProps> = ({
   }
 
   const changeTrackingNumberData = async (e: React.FormEvent) => {
-    if ((emptyTotes.length === 0) && (stIds.length === 0) && (tnIds.length === 0)) {
-        setStartTime(new Date());
+    if (emptyTotes.length === 0 && stIds.length === 0 && tnIds.length === 0) {
+      setStartTime(new Date());
     }
     if (locationTag === "99" && !/^(SAP_)/.test(enteredTracking)) {
       const createRecord = {
@@ -51,56 +50,59 @@ const OutboundForm: React.FC<OutboundFormProps> = ({
       setEnteredTrackingNumbers([...enteredTrackingNumbers, createdTn]);
       setTnIds([...tnIds, createdTn.id]);
     } else if (locationTag === "133" && !/^(SAP_)/.test(enteredTracking)) {
-        const tnIndex = trackingNumbers.findIndex((obj) => obj.TrackingNumber === enteredTracking);
-        if (tnIndex === -1) {
-            setShowAlert(true);
-            setEnteredTracking('');
-            return;
-        }
-        const updateRecord = {
-            TrackingNumber: trackingNumbers[tnIndex].TrackingNumber,
-            Outbound133: new Date().toLocaleString(),
-            alias: localStorage.getItem("id") as string
-        }
-        const updatedTn = await TNUpdate(trackingNumbers[tnIndex].id, updateRecord);
-        setEnteredTrackingNumbers([...enteredTrackingNumbers, updatedTn]);
-        setTnIds([...tnIds, updatedTn.id]);
-    } else if (locationTag === "99" && /^(SAP_)/.test(enteredTracking)) {
-        setFrom99To133(true);
-        setEmptyTotes([...emptyTotes, enteredTracking]);
-    } else if (locationTag === "133" && /^(SAP_)/.test(enteredTracking)) {
-        const stIndex = sapTotes.findIndex((obj) => obj.ToteID === enteredTracking);
-        if(stIndex === -1) {
-            setShowAlert(true);
-            setEnteredTracking('');
-            return;
-        }
-        setStIds([...stIds, sapTotes[stIndex].id]);
-        setEnteredSapTotes([...enteredSapTotes, sapTotes[stIndex]]);
-    } else {
+      const tnIndex = trackingNumbers.findIndex(
+        (obj) => obj.TrackingNumber === enteredTracking
+      );
+      if (tnIndex === -1) {
         setShowAlert(true);
+        setEnteredTracking("");
+        return;
+      }
+      const updateRecord = {
+        TrackingNumber: trackingNumbers[tnIndex].TrackingNumber,
+        Outbound133: new Date().toLocaleString(),
+        alias: localStorage.getItem("id") as string,
+      };
+      const updatedTn = await TNUpdate(
+        trackingNumbers[tnIndex].id,
+        updateRecord
+      );
+      setEnteredTrackingNumbers([...enteredTrackingNumbers, updatedTn]);
+      setTnIds([...tnIds, updatedTn.id]);
+    } else if (/^(SAP_)/.test(enteredTracking)) {
+      const stIndex = sapTotes.findIndex(
+        (obj) => obj.ToteID === enteredTracking
+      );
+      if (stIndex === -1) {
+        setShowAlert(true);
+        setEnteredTracking("");
+        return;
+      }
+      setStIds([...stIds, sapTotes[stIndex].id]);
+      setEnteredSapTotes([...enteredSapTotes, sapTotes[stIndex]]);
+    } else {
+      setShowAlert(true);
     }
   };
 
   const submitContainer = async (e: React.FormEvent) => {
     const enteredContainer = {
-        ContainerID: containerId,
-        StartTime: startTime,
-        StagedTime: new Date(),
-        TrackingNumbers: tnIds,
-        SapTotes: stIds,
-        alias: localStorage.getItem("id") as string
-    }
+      ContainerID: containerId,
+      StartTime: startTime,
+      StagedTime: new Date(),
+      TrackingNumbers: tnIds,
+      SapTotes: stIds,
+      alias: localStorage.getItem("id") as string,
+    };
     await ContCreate(enteredContainer);
-    setContainerId('');
-    setLocationTag('');
+    setContainerId("");
+    setLocationTag("");
     setStIds([]);
     setTnIds([]);
     setEnteredTrackingNumbers([]);
-    setFrom99To133(false);
     setEmptyTotes([]);
     setEnteredSapTotes([]);
-  }
+  };
 
   function handleClose(): void {
     setShowAlert(false);
@@ -148,25 +150,21 @@ const OutboundForm: React.FC<OutboundFormProps> = ({
           onChange={(e) => setEnteredTracking(e.target.value)}
         />
       </Form>
-      {!from99To133 ? (
-        <ListGroup>
-            {enteredSapTotes.map((SapTote) => (
-                <ListGroup.Item variant="dark" key={SapTote.id}>
-                    <DisplaySapTote SapTote={SapTote}/>
-                </ListGroup.Item>
-            ))}
-        </ListGroup>
-      ): (
-        <EmptyToteList emptyTotes={emptyTotes}/>
-      )}
+      <ListGroup>
+        {enteredSapTotes.map((SapTote) => (
+          <ListGroup.Item variant="dark" key={SapTote.id}>
+            <DisplaySapTote SapTote={SapTote} />
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
       <TrackingNumberList trackingNumbersList={enteredTrackingNumbers} />
       <Modal centered show={showAlert} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>Tracking Number Not Valid</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          The tracking number that you have entered has an error please make sure you are 
-          scanning the correct code.
+          The tracking number that you have entered has an error please make
+          sure you are scanning the correct code.
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
