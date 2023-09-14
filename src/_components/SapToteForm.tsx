e4449b045de598ad7a02e5436a3958b23e60c55b
Dizzion <dizzander43@gmail.com>
2023-09-14 08:47:59 -0700

    update handling unit function created

--- src/_components/SapToteForm.tsx
+++ src/_components/SapToteForm.tsx
@@ -1,12 +1,39 @@
 'use client'
+import { HUUpdate } from '@/utils/pocketbase';
 import { RecordModel } from 'pocketbase';
-import React from 'react'
+import React, { useState } from 'react'
 
 interface SapToteFormProps {
     handlingUnits: RecordModel[];
 }
 
 const SapToteForm: React.FC<SapToteFormProps> = ({ handlingUnits }) => {
+    const [uid, setUid] = useState(`SAP_${Date.now()}-${Math.floor(Math.random() * 10000)}`);
+    const [enteredHandlingUnits, setEnteredHandlingUnits] = useState<number[]>([]);
+    const [enteredHandlingUnit, setEnteredHandlingUnit] = useState('');
+    const [showAlert, setShowAlert] = useState(false);
+
+    function handleClose() {
+        setShowAlert(false);
+    }
+
+    const updateHandlingUnits = async (e: React.FormEvent) => {
+        e.preventDefault();
+        const enteredIndex = handlingUnits.findIndex((obj) => obj.HU === enteredHandlingUnit);
+        if ((enteredIndex === -1) || (/^(199|133|299|233)/.test(enteredHandlingUnit))) {
+            setShowAlert(true);
+            return;
+        } 
+        const HUtoUpdate = {
+            HU: handlingUnits[enteredIndex].HU,
+            ToQI: handlingUnits[enteredIndex].ToQI,
+            StagedTime: new Date(),
+            alias: localStorage.getItem("id") as string
+        }
+        const updatedHU = await HUUpdate(handlingUnits[enteredIndex].id, HUtoUpdate);
+        setEnteredHandlingUnits([...enteredHandlingUnits, updatedHU.HU]);
+    }
+
   return (
     <div>SapToteForm</div>
   )
