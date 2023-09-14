828eff4eed6bd04b0d535ea82519ff31b272f27c
Dizzion <dizzander43@gmail.com>
2023-09-14 15:19:12 -0700

    Inbound Form next

---
+++ src/_components/InboundForm.tsx
@@ -0,0 +1,15 @@
+'use client';
+import { RecordModel } from 'pocketbase';
+import React from 'react';
+
+interface InboundFormProps {
+    Containers: RecordModel[];
+}
+
+const InboundForm: React.FC<InboundFormProps> = ({ Containers }) => {
+  return (
+    <div></div>
+  );
+};
+
+export default InboundForm;
