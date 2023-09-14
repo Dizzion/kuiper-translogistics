9e0f3b8bd6071de12fdebc84bfd2675d298f2d43
Dizzion <dizzander43@gmail.com>
2023-09-14 15:46:56 -0700

    Inbound started states set

--- src/_components/InboundForm.tsx
+++ src/_components/InboundForm.tsx
@@ -1,14 +1,20 @@
 'use client';
 import { RecordModel } from 'pocketbase';
-import React from 'react';
+import React, { useState } from 'react';
 
 interface InboundFormProps {
-    Containers: RecordModel[];
+    containers: RecordModel[];
+    trackingNumbers: RecordModel[];
+    sapTotes: RecordModel[];
 }
 
-const InboundForm: React.FC<InboundFormProps> = ({ Containers }) => {
+const InboundForm: React.FC<InboundFormProps> = ({ containers, trackingNumbers, sapTotes }) => {
+    const [enteredContId, setEnteredContId] = useState('');
+    const [locationTag, setLocationTag] = useState('');
+    const [enteredTracking, setEnteredTracking] = useState('');
+    const [showAlert, setShowAlert] = useState(false);
   return (
-    <div></div>
+    <></>
   );
 };
 
--- src/app/Translogistics/Inbound/page.tsx
+++ src/app/Translogistics/Inbound/page.tsx
@@ -1,9 +1,19 @@
+import InboundForm from "@/_components/InboundForm";
+import { ContGetAll, STGetAll, TNGetAll } from "@/utils/pocketbase";
+import { RecordModel } from "pocketbase";
 import React from "react";
 
-const Inbound = () => {
-    return (
-        <div>Inbound Form</div>
-    );
+const Inbound = async () => {
+  const containers = (await ContGetAll()) as RecordModel;
+  const trackingNumbers = (await TNGetAll()) as RecordModel;
+  const sapTotes = (await STGetAll()) as RecordModel;
+  return (
+    <InboundForm
+      containers={containers.items}
+      trackingNumbers={trackingNumbers.items}
+      sapTotes={sapTotes.items}
+    />
+  );
 };
 
 export default Inbound;
