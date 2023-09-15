72d43f7a7dea2e5d7806f6bac71496cff2770bd2
Dizzion <gofrodude_197@hotmail.com>
2023-09-15 01:16:21 -0700

    display logic written

--- src/_components/DisplayContainer.tsx
+++ src/_components/DisplayContainer.tsx
@@ -1,5 +1,6 @@
 import { RecordModel } from 'pocketbase'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Container, Row, Col, ListGroup } from 'react-bootstrap';
 
 interface DisplayContainerProps {
     container: RecordModel;
@@ -8,8 +9,45 @@ interface DisplayContainerProps {
 }
 
 const DisplayContainer: React.FC<DisplayContainerProps> = ({ container, trackingNumbers, sapTotes }) => {
+    const [trackingDataList, setTrackingDataList] = useState<string[]>([]);
+    const [column1, setColumn1] = useState<string[]>([]);
+    const [column2, setColumn2] = useState<string[]>([]);
+
+    useEffect(() => {
+        container.TrackingNumbers.forEach((id: string) => {
+            const index = trackingNumbers.findIndex((tn) => tn.id === id);
+            setTrackingDataList([...trackingDataList, trackingNumbers[index].TrackingNumber]);
+        });
+        container.SapTotes.forEach((id: string) => {
+            const index = sapTotes.findIndex((st) => st.id === id);
+            setTrackingDataList([...trackingDataList, sapTotes[index].ToteID]);
+        });
+        setColumn1(trackingDataList.filter((_, index) => index % 2 === 0));
+        setColumn2(trackingDataList.filter((_, index) => index % 2 !== 0));
+    }, []);
   return (
-    <div>DisplayContainer</div>
+    <Container>
+        <Row>
+          <Col>
+            <ListGroup>
+              {column1.map((trackingData) => (
+                <ListGroup.Item variant="dark" key={trackingData}>
+                  {trackingData}
+                </ListGroup.Item>
+              ))}
+            </ListGroup>
+          </Col>
+          <Col>
+            <ListGroup>
+              {column2.map((trackingData) => (
+                <ListGroup.Item variant="dark" key={trackingData}>
+                  {trackingData}
+                </ListGroup.Item>
+              ))}
+            </ListGroup>
+          </Col>
+        </Row>
+      </Container>
   )
 }
 
