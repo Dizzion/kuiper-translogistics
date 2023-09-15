5bee39f5d6f3c6b7efc5628e2c5164378882cb0a
Dizzion <gofrodude_197@hotmail.com>
2023-09-15 02:20:14 -0700

    dashboard landing and nav styling

--- src/_components/TranslogisticsNav.tsx
+++ src/_components/TranslogisticsNav.tsx
@@ -1,26 +1,50 @@
 "use client";
 import { usePathname } from "next/navigation";
-import React from "react";
+import React, { useState } from "react";
 import { Nav } from "react-bootstrap";
 
 const TranslogisticsNav = () => {
   const pathname = usePathname();
+  const [hoveredLink, setHoveredLink] = useState<string | null>(null);
+
+  const handleMouseEnter = (linkName: string) => {
+    setHoveredLink(linkName);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredLink(null);
+  };
+
+  const links = [
+    { name: 'Warehouse Metrics Dashboard', href: '/Translogistics/Dashboard' },
+    { name: 'Transportation', href: '/Translogistics/Transportation' },
+    { name: 'Outbound', href: '/Translogistics/Outbound' },
+    { name: 'Inbound', href: '/Translogistics/Inbound' },
+    { name: 'Receiving', href: '/Translogistics/Receiving' },
+    { name: 'SapTote', href: '/Translogistics/SapTote' },
+    { name: 'Delivery', href: '/Translogistics/Delivery' },
+  ];
   return (
     <Nav
       variant="pills"
       className="text-center flex-column"
       style={{ backgroundColor: "#5d5f63" }}
     >
-      <Nav.Item>
+      {links.map((link) => (
+        <Nav.Item key={link.name}
+          onMouseEnter={() => handleMouseEnter(link.name)}
+          onMouseLeave={handleMouseLeave}>
         <Nav.Link
-          href="/Translogistics"
-          className={pathname == "/Translogistics" ? "active" : ""}
-          style={{ color: "white" }}
+          href={link.href}
+          className={pathname == link.href ? "active" : "non-active"}
+          style={{ backgroundColor: hoveredLink === link.name ? '#007bff' : '', color: "white" }}
         >
-          Warehouse Metrics Dashboard
+          {link.name}
         </Nav.Link>
       </Nav.Item>
-      <Nav.Item>
+      ))}
+      
+      {/* <Nav.Item>
         <Nav.Link
           href="/Translogistics/Transportation"
           style={{color: 'white'}}
@@ -75,7 +99,7 @@ const TranslogisticsNav = () => {
         >
           Delivery
         </Nav.Link>
-      </Nav.Item>
+      </Nav.Item> */}
     </Nav>
   );
 };
