8e132904961c675bf95fd89511ed0312704c3b8a
Dizzion <gofrodude_197@hotmail.com>
2023-09-15 01:24:35 -0700

    display written

--- src/_components/DisplayContainer.tsx
+++ src/_components/DisplayContainer.tsx
@@ -1,54 +1,75 @@
-import { RecordModel } from 'pocketbase'
-import React, { useEffect, useState } from 'react'
-import { Container, Row, Col, ListGroup } from 'react-bootstrap';
+import { RecordModel } from "pocketbase";
+import React, { useEffect, useState } from "react";
+import { Container, Row, Col, ListGroup } from "react-bootstrap";
 
 interface DisplayContainerProps {
-    container: RecordModel;
-    trackingNumbers: RecordModel[];
-    sapTotes: RecordModel[];
+  container: RecordModel;
+  trackingNumbers: RecordModel[];
+  sapTotes: RecordModel[];
 }
 
-const DisplayContainer: React.FC<DisplayContainerProps> = ({ container, trackingNumbers, sapTotes }) => {
-    const [trackingDataList, setTrackingDataList] = useState<string[]>([]);
-    const [column1, setColumn1] = useState<string[]>([]);
-    const [column2, setColumn2] = useState<string[]>([]);
+const DisplayContainer: React.FC<DisplayContainerProps> = ({
+  container,
+  trackingNumbers,
+  sapTotes,
+}) => {
+  const [trackingDataList, setTrackingDataList] = useState<string[]>([]);
+  const [column1, setColumn1] = useState<string[]>([]);
+  const [column2, setColumn2] = useState<string[]>([]);
+  const [isExpanded, setIsExpanded] = useState(false);
 
-    useEffect(() => {
-        container.TrackingNumbers.forEach((id: string) => {
-            const index = trackingNumbers.findIndex((tn) => tn.id === id);
-            setTrackingDataList([...trackingDataList, trackingNumbers[index].TrackingNumber]);
-        });
-        container.SapTotes.forEach((id: string) => {
-            const index = sapTotes.findIndex((st) => st.id === id);
-            setTrackingDataList([...trackingDataList, sapTotes[index].ToteID]);
-        });
-        setColumn1(trackingDataList.filter((_, index) => index % 2 === 0));
-        setColumn2(trackingDataList.filter((_, index) => index % 2 !== 0));
-    }, []);
+  useEffect(() => {
+    container.TrackingNumbers.forEach((id: string) => {
+      const index = trackingNumbers.findIndex((tn) => tn.id === id);
+      setTrackingDataList([
+        ...trackingDataList,
+        trackingNumbers[index].TrackingNumber,
+      ]);
+    });
+    container.SapTotes.forEach((id: string) => {
+      const index = sapTotes.findIndex((st) => st.id === id);
+      setTrackingDataList([...trackingDataList, sapTotes[index].ToteID]);
+    });
+    setColumn1(trackingDataList.filter((_, index) => index % 2 === 0));
+    setColumn2(trackingDataList.filter((_, index) => index % 2 !== 0));
+  }, []);
+
+  const toggleExpandCollapse = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   return (
-    <Container>
-        <Row>
-          <Col>
-            <ListGroup>
-              {column1.map((trackingData) => (
-                <ListGroup.Item variant="dark" key={trackingData}>
-                  {trackingData}
-                </ListGroup.Item>
-              ))}
-            </ListGroup>
-          </Col>
-          <Col>
-            <ListGroup>
-              {column2.map((trackingData) => (
-                <ListGroup.Item variant="dark" key={trackingData}>
-                  {trackingData}
-                </ListGroup.Item>
-              ))}
-            </ListGroup>
-          </Col>
-        </Row>
-      </Container>
-  )
-}
+    <ListGroup.Item
+      variant={isExpanded ? "light" : "dark"}
+      onClick={toggleExpandCollapse}
+    >
+      Container ID: {container.ContainerID}
+      {isExpanded && (
+        <Container>
+          <Row>
+            <Col>
+              <ListGroup>
+                {column1.map((trackingData) => (
+                  <ListGroup.Item variant="dark" key={trackingData}>
+                    {trackingData}
+                  </ListGroup.Item>
+                ))}
+              </ListGroup>
+            </Col>
+            <Col>
+              <ListGroup>
+                {column2.map((trackingData) => (
+                  <ListGroup.Item variant="dark" key={trackingData}>
+                    {trackingData}
+                  </ListGroup.Item>
+                ))}
+              </ListGroup>
+            </Col>
+          </Row>
+        </Container>
+      )}
+    </ListGroup.Item>
+  );
+};
 
-export default DisplayContainer
+export default DisplayContainer;
