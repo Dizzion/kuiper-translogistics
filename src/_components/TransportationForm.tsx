15e592ab20d1c8ebfb4ce96b83f94ca2e644849a
Dizzion <gofrodude_197@hotmail.com>
2023-09-15 01:43:17 -0700

    loading form in

--- src/_components/TransportationForm.tsx
+++ src/_components/TransportationForm.tsx
@@ -2,6 +2,7 @@
 import { CreateTruck, UpdateTruck } from "@/utils/pocketbase";
 import { RecordModel } from "pocketbase";
 import React, { useState } from "react";
+import { Form, Row, Col, Button, Modal, Card, CardGroup } from "react-bootstrap";
 
 interface TransportationFormProps {
   trucks: RecordModel[];
@@ -26,6 +27,7 @@ const TransportationForm: React.FC<TransportationFormProps> = ({
   const [locationTag, setLocationTag] = useState("");
   const [loadOrUnload, setLoadOrUnload] = useState("");
   const [enteredContainer, setEnteredContainer] = useState("");
+  const [showAlert, setShowAlert] = useState(false);
   const [arrivalTime, setArrivalTime] = useState<Date>();
   const [containerIds, setContainerIds] = useState<string[]>([]);
   const [containerList, setContainerList] = useState<RecordModel[]>([]);
@@ -98,6 +100,7 @@ const TransportationForm: React.FC<TransportationFormProps> = ({
   };
 
   const submitTruck = async (e: React.FormEvent) => {
+    e.preventDefault();
     if (loadOrUnload === "unload") {
       const record = {
         TruckID: truckId,
@@ -189,17 +192,125 @@ const TransportationForm: React.FC<TransportationFormProps> = ({
     }
   };
 
-  const updateEnteredContainer = (value: string) => {
-    setEnteredContainer(value);
+  const updateEnteredContainer = (e: React.FormEvent) => {
+    e.preventDefault();
     const contIndex = containers.findIndex(
       (obj) => obj.ContainerID === enteredContainer
     );
-    if (contIndex !== -1) {
+    if (contIndex !== -1 && !(containerList.some((cont) => cont.ContainerID === enteredContainer))) {
       setContainerList([...containerList, containers[contIndex]]);
+      return;
     }
+    setShowAlert(true);
   };
 
-  return <div>TransportationForm</div>;
+  function handleClose(): void {
+    setShowAlert(false);
+  }
+
+  return (
+    <>
+      <Form onSubmit={submitTruck} className="text-white">
+        <Form.Group>
+          <Row>
+            <Col>
+              <Form.Label>Location</Form.Label>
+              <Form.Select
+            size="lg"
+            required
+            value={locationTag}
+            onChange={(e) => setLocationTag(e.target.value)}
+          >
+            <option>Select Your Location</option>
+            <option value="99">SEA99</option>
+            <option value="133">SEA133</option>
+          </Form.Select>
+            </Col>
+            <Col>
+              <Form.Label>Truck ID</Form.Label>
+              <Form.Select
+                placeholder="A, B, C or D?"
+                size="lg"
+                value={truckId}
+                required
+                onChange={(e) => setTruckId(e.target.value)}
+              >
+                <option>Select Truck ID</option>
+                <option value="A">A</option>
+                <option value="B">B</option>
+                <option value="C">C</option>
+                <option value="D">D</option>
+              </Form.Select>
+            </Col>
+          </Row>
+          <Form.Label>Loading or Unloading?</Form.Label>
+          <Form.Select
+            required
+            onChange={(e) => setLoadOrUnload(e.target.value)}
+          >
+            <option>Select Load or Unload</option>
+            <option value="load">Load</option>
+            <option value="unload">Unload</option>
+          </Form.Select>
+        </Form.Group>
+        {loadOrUnload === "load" ? (
+          <Button type="submit" variant="outline-dark">
+            Depart Load
+          </Button>
+        ) : (
+          <Button type="submit" variant="outline-dark">
+            Finish Unload
+          </Button>
+        )}
+      </Form>
+      <Modal centered show={showAlert} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Double Scan</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        Make sure you aren't scanning the same Container twice.
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant='secondary' onClick={handleClose}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+      {loadOrUnload === "load" ? (
+        <>
+        <CardGroup>
+          <Card bg="secondary" style={{ width: "18rem" }}>
+            <Card.Header className="text-white">Processing</Card.Header>
+            <Card.Body key={timers[1].id}>
+            Elapsed Time for Timer {timers[1].id}: {timers[1].elapsedTime} seconds
+            <Button variant="outline-light" onClick={() => startTimer(timers[1].id)}>Start</Button>
+            <Button variant="outline-light" onClick={() => stopTimer(timers[1].id)}>Stop</Button>
+            </Card.Body>
+          </Card>
+          <Card bg="secondary" style={{ width: "18rem" }}>
+            <Card.Header className="text-white">Loading</Card.Header>
+            <Card.Body>
+              
+            </Card.Body>
+          </Card>
+        </CardGroup>
+        <Form onSubmit={updateEnteredContainer}>
+          <Form.Label>Enter Containers</Form.Label>
+          <Form.Control
+            placeholder="Container ID"
+            size="lg"
+            value={enteredContainer}
+            onChange={(e) => setEnteredContainer(e.target.value)}
+          />
+        </Form>
+      </>
+      ) : (
+        <UnloadingForm
+          bins={enteredBins}
+          onChangeEnteredBin={handleChangeEnteredBin}
+        />
+      )}
+      <BinList bins={enteredBins} />
+    </>
+  );;
 };
 
 export default TransportationForm;
