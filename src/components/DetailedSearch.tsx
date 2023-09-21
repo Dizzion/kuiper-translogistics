cae5b492e83f5322cbac9d9a29e42e192ebd1d7a
Dizzion <dizzander43@gmail.com>
2023-09-21 16:22:41 -0700

    getting detailed search finished

--- src/components/DetailedSearch.tsx
+++ src/components/DetailedSearch.tsx
@@ -1,17 +1,65 @@
-'use client'
-import { TNGetByTN } from '@/utils/pocketbase';
-import React, { useState } from 'react';
-import { Form } from 'react-bootstrap';
+"use client";
+import { TNGetByTN } from "@/utils/pocketbase";
+import { RecordModel } from "pocketbase";
+import React, { useState } from "react";
+import { Button, Form, Modal } from "react-bootstrap";
+import DisplayDetails from "./DisplayDetails";
 
-const DetailedSearch = async () => {
-  const [enteredTN, setEnteredTN] = useState('');
+const DetailedSearch = () => {
+  const [enteredTN, setEnteredTN] = useState("");
+  const [showAlert, setShowAlert] = useState(false);
+  const [fullTNData, setFullTNData] = useState<RecordModel>();
 
-  const searchTNDetails = (e: React.FocusEvent) => {
+  const searchTNDetails = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const searchedTN = await TNGetByTN(enteredTN);
+    if (searchedTN === undefined) {
+      setShowAlert(true);
+      setEnteredTN("");
+      return;
+    }
+    setFullTNData(searchedTN.items[0]);
+    setEnteredTN("");
+  };
 
+  function handleClose() {
+    setShowAlert(false);
   }
 
   return (
-    <><Form></Form></>
+    <>
+      <Form onSubmit={searchTNDetails}>
+        <Form.Label className="text-white">
+          Search Detailed Tracking Number Info
+        </Form.Label>
+        <Form.Control
+          type="trackingNumber"
+          value={enteredTN}
+          onChange={(e) => setEnteredTN(e.target.value)}
+        />
+        <Button type="submit" variant="outline-light">
+          Search
+        </Button>
+      </Form>
+      <Modal centered show={showAlert} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Tracking Number Not Valid</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          The tracking number that you have entered was not found.
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      {fullTNData !== undefined ? (
+        <DisplayDetails fullTNData={fullTNData} />
+      ) : (
+        <></>
+      )}
+    </>
   );
 };
 
--- src/components/DisplayDetails.tsx
+++ src/components/DisplayDetails.tsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import { RecordModel } from "pocketbase";
+import React, { useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
-function DisplayDetails() {
-  return (
-    <div>DisplayDetails</div>
-  )
+interface DisplayDetailsProps {
+  fullTNData: RecordModel;
 }
 
-export default DisplayDetails
+const DisplayDetails: React.FC<DisplayDetailsProps> = ({ fullTNData }) => {
+  useEffect(() => {}, [fullTNData]);
+  return (
+    <Container className="text-white">
+      <Row>
+        <h1>Tracking Number: {fullTNData.TrackingNumber}</h1>
+      </Row>
+      {fullTNData.Outbound99 ? (
+        <Row>
+          <Col>
+            <h3>Outbound 99: {fullTNData.Outbound99}</h3>
+          </Col>
+          <Col>Inbound 133: {fullTNData.Inbound133}</Col>
+        </Row>
+      ) : (
+        <></>
+      )}
+      {fullTNData.Delivered ? (
+        <Row>
+          <h2>Arrived at 133?: {fullTNData.Delivered}</h2>
+        </Row>
+      ) : (
+        <></>
+      )}
+      <Row>
+        <h3>Arrived at 133?: {fullTNData.Received133}</h3>
+      </Row>
+      <Row>
+        <Col>
+          <h3>Outbound 133: {fullTNData.Outbound133}</h3>
+        </Col>
+        <Col>Inbound 133: {fullTNData.Inbound133}</Col>
+      </Row>
+      <Row>
+        <Col>
+          <h4>Requestor: {fullTNData.full_name}</h4>
+        </Col>
+        <Col>
+          <h4>Requestor Location: {fullTNData.defualt_location}</h4>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <h5>Coupa Po and Lines: {fullTNData.CoupaPOLines}</h5>
+        </Col>
+        <Col>
+          <h5>SAP: {fullTNData.SAP}</h5>
+        </Col>
+      </Row>
+      {fullTNData.SAP === "Yes" ? <Row></Row> : <></>}
+    </Container>
+  );
+};
+
+export default DisplayDetails;
