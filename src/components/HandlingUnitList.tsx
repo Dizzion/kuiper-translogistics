27feec47a7232b5b50b9197efc7ee5b8910795c6
Dizzion <dizzander43@gmail.com>
2023-09-25 12:51:02 -0700

    update to refresh list

--- src/components/HandlingUnitList.tsx
+++ src/components/HandlingUnitList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Col, Container, ListGroup, Row } from "react-bootstrap";
 
 interface HandlingUnitListProps {
@@ -8,8 +8,16 @@ interface HandlingUnitListProps {
 const HandlingUnitList: React.FC<HandlingUnitListProps> = ({
   handlingUnits,
 }) => {
-  const column1 = handlingUnits.filter((_, index) => index % 2 === 0);
-  const column2 = handlingUnits.filter((_, index) => index % 2 !== 0);
+  const [column1, setColumn1] = useState(
+    handlingUnits.filter((_, index) => index % 2 === 0)
+  );
+  const [column2, setColumn2] = useState(
+    handlingUnits.filter((_, index) => index % 2 !== 0)
+  );
+  useEffect(() => {
+    setColumn1(handlingUnits.filter((_, index) => index % 2 === 0));
+    setColumn2(handlingUnits.filter((_, index) => index % 2 !== 0));
+  }, [handlingUnits]);
   return (
     <div>
       <h4 className="text-white">Handling Units: {handlingUnits.length}</h4>
