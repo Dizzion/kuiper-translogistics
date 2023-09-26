b3ef756c6c539a7c091d89b3fceb01bad542e69f
Dizzion <dizzander43@gmail.com>
2023-09-26 16:08:32 -0700

    keep info option while receiving

--- src/components/ReceivingForm.tsx
+++ src/components/ReceivingForm.tsx
@@ -60,6 +60,7 @@ const ReceivingForm: React.FC<ReceivingFormProps> = ({ trackingNumbers }) => {
   const [showAlert, setShowAlert] = useState(false);
   const [showAlert2, setShowAlert2] = useState(false);
   const [showAlert3, setShowAlert3] = useState(false);
+  const [showAlert4, setShowAlert4] = useState(false);
   const [showSubmit, setShowSubmit] = useState(false);
   const [enteredHUs, setEnteredHUs] = useState<number[]>([]);
   const [enteredHU, setEnteredHU] = useState("");
@@ -172,17 +173,13 @@ const ReceivingForm: React.FC<ReceivingFormProps> = ({ trackingNumbers }) => {
     const searchedTN = await TNGetByTN(enteredTrackingNumber);
 
     if (searchedTN.items.length > 0) {
-      if (searchedTN.items[0].Inbound133 && searchedTN.items[0].Delivered) {
+      if (searchedTN.items[0].Inbound133.length === 0 && searchedTN.items[0].Delivered.length === 0) {
         setShowAlert2(true);
         return;
       }
       const updatedTrackingNumber = {
         TrackingNumber: enteredTrackingNumber,
-        Outbound99: searchedTN.items[0].Outbound99,
-        Inbound133: searchedTN.items[0].Inbound133,
         Received133: newTimestamp,
-        Outbound133: searchedTN.items[0].Outbound133,
-        Inbound99: searchedTN.items[0].Inbound99,
         full_name: requestor.name,
         default_location: requestor.building,
         CoupaPOLines: requestor.coupaPoLines,
@@ -221,17 +218,7 @@ const ReceivingForm: React.FC<ReceivingFormProps> = ({ trackingNumbers }) => {
       (requestor.freight === "Yes" || requestor.freight === "No")
     ) {
       await updateAsReceived(enteredTrackingNumber, requestor);
-      setEnteredHUs([]);
-      setEnteredTrackingNumber("");
-      setRequestor({
-        name: "",
-        building: "",
-        inventory: requestor.inventory,
-        freight: requestor.freight,
-        jira: "",
-        handlingUnits: [],
-        coupaPoLines: "",
-      });
+      setShowAlert4(true);
       return;
     }
     setShowAlert3(true);
@@ -245,6 +232,27 @@ const ReceivingForm: React.FC<ReceivingFormProps> = ({ trackingNumbers }) => {
     setGenerateFullName(false);
     setShowSubmit(true);
   };
+
+  function keepInfo(): void {
+    setShowAlert4(false);
+    setEnteredHUs([]);
+    setEnteredTrackingNumber("");
+  }
+
+  function clearForm(): void {
+    setShowAlert4(false);
+    setEnteredHUs([]);
+    setEnteredTrackingNumber("");
+    setRequestor({
+      name: "",
+      building: "",
+      inventory: requestor.inventory,
+      freight: requestor.freight,
+      jira: "",
+      handlingUnits: [],
+      coupaPoLines: "",
+    });
+  }
 
   function handleClose(): void {
     setShowAlert(false);
@@ -516,9 +524,22 @@ const ReceivingForm: React.FC<ReceivingFormProps> = ({ trackingNumbers }) => {
           </Button>
         </Modal.Footer>
       </Modal>
+      <Modal centered show={showAlert4}>
+        <Modal.Body>
+          Would you like to reset the form or keep the information?
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={keepInfo}>
+            Keep Info
+          </Button>
+          <Button variant="secondary" onClick={clearForm}>
+            Clear
+          </Button>
+        </Modal.Footer>
+      </Modal>
       <Modal centered show={showAlert2} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Missing Pervious Scan</Modal.Title>
+          <Modal.Title>Missing Previous Scan</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           Pervious Scan hasn't been captured please make sure to follow the
