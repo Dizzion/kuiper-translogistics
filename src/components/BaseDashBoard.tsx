14527b977e94bf5c69df05c82f48a0e820d0960d
Dizzion <gofrodude_197@hotmail.com>
2023-10-03 20:50:45 -0700

    first chart

--- src/components/BaseDashBoard.tsx
+++ src/components/BaseDashBoard.tsx
@@ -12,6 +12,7 @@ import {
 } from "chart.js";
 import { Chart } from "react-chartjs-2";
 import { Row } from "react-bootstrap";
+import { RecordModel } from "pocketbase";
 
 ChartJS.register(
   LinearScale,
@@ -23,7 +24,37 @@ ChartJS.register(
   Tooltip
 );
 
-const labels = ["January", "February", "March", "April", "May"];
+const date = new Date();
+let dailyAmount: number[] = [];
+
+("use server");
+async function getData() {
+  const dateFind = new Date();
+  while (dailyAmount.length < 6) {
+    let i = 0;
+    const res = await fetch(
+      `${
+        process.env.APP_SERVER
+      }/api/collections/TrackingNumbers/records?perPage=500&Received133>=${new Date(dateFind.setDate(dateFind.getDate() - i)).toDateString()}`,
+      { cache: "no-store" }
+    );
+    const packages: RecordModel = await res.json() as unknown as RecordModel;
+    if (packages.items[0]) {
+      dailyAmount.push(packages.items[0].length);
+      if (i < 1) {
+        i++;
+      }
+    }
+  }
+}
+
+const labels = [
+  new Date(date.setDate(date.getDate())).toDateString(),
+  new Date(date.setDate(date.getDate() - 1)).toDateString(),
+  new Date(date.setDate(date.getDate() - 1)).toDateString(),
+  new Date(date.setDate(date.getDate() - 1)).toDateString(),
+  new Date(date.setDate(date.getDate() - 1)).toDateString(),
+];
 
 export const data = {
   labels,
@@ -35,13 +66,13 @@ export const data = {
       backgroundColor: "rgba(255, 99, 132, 0.7)",
       borderWidth: 2,
       fill: false,
-      data: [13545, 15467, 17657, 12546, 15768],
+      data: [0, 0, 0, 0, 0],
     },
     {
       type: "bar" as const,
       label: "Dataset 2",
       backgroundColor: "#5f90f1",
-      data: [24565, 28609, 18569, 19708, 21098],
+      data: [dailyAmount[0], dailyAmount[1], dailyAmount[2], dailyAmount[3], dailyAmount[4]],
       boarderColor: "white",
       boarderWidth: 2,
     },
@@ -85,31 +116,32 @@ const BaseDashBoard = async () => {
   const chartRef = useRef<ChartJS>(null);
 
   useEffect(() => {
+    getData();
     const chart = chartRef.current;
     if (chart) {
       chart.config.options = {
         scales: {
           x: {
             grid: {
-              color: '#bfc1c5',
+              color: "#bfc1c5",
             },
             ticks: {
-              color: '#bfc1c5',
+              color: "#bfc1c5",
             },
           },
           y: {
             grid: {
-              color: '#bfc1c5',
+              color: "#bfc1c5",
             },
             ticks: {
-              color: '#bfc1c5',
+              color: "#bfc1c5",
             },
           },
         },
         plugins: {
           legend: {
             labels: {
-              color: '#bfc1c5', 
+              color: "#bfc1c5",
             },
           },
         },
